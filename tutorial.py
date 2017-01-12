8295194f78b83cdd58766e119b9f3e5c98b91cc2
Nic <nicolas.baldenko@gmail.com>
2017-01-12 09:27:40 +0100

    load data and view types

--- tutorial.py
+++ tutorial.py
@@ -6,7 +6,7 @@ import random as rnd
 # visualization
 import seaborn as sns
 import matplotlib.pyplot as plt
-%matplotlib inline
+#matplotlib inline
 
 # machine learning
 from sklearn.linear_model import LogisticRegression
@@ -14,3 +14,14 @@ from sklearn.svm import SVC, LinearSVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
+
+# read titanic training & test csv files as a pandas DataFrame
+train_df = pd.read_csv('/Users/nicolasbaldenko/Documents/Portfolio/Data Science/Titanic/Data/train.csv')
+test_df = pd.read_csv('/Users/nicolasbaldenko/Documents/Portfolio/Data Science/Titanic/Data/test.csv')
+
+train_df.info()
+print("_"*40)
+test_df.info()
+
+#print(train_df.columns.values)
+#print(test_df.columns.values)
